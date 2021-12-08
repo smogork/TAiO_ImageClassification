65cefa8ae07513fd4dd536001d75948c9b770be9
smogork <krzysztof.smogor@wp.pl>
2021-12-08 21:38:16 +0100

    Poprawka implementacji feature max i min

--- feature/simple_features/max_feature.py
+++ feature/simple_features/max_feature.py
@@ -3,6 +3,8 @@
 """
 Moduł zawiera klasę wyliczajcą numer  komórki, która nie jest biała
 """
+import numpy as np
+
 from bitmap.bitmap_grayscale import BitmapGrayscale
 from feature.feature import Feature
 
@@ -14,10 +16,10 @@ class MaxFeature(Feature):
     """
 
     def __init__(self):
-        self.__tab = []
+        self.__tab = None
 
     def calculate(self) -> float:
-        if len(self.__tab) == 0:
+        if self.__tab is None or len(self.__tab) == 0:
             raise RuntimeError("Run prepare() before calculate()")
 
         maximum = -1
@@ -26,9 +28,11 @@ class MaxFeature(Feature):
             if cell < BitmapGrayscale.White:
                 maximum = i
             i += 1
+        self.__tab = None
         return maximum
 
     def prepare(self, bitmap: BitmapGrayscale) -> None:
+        self.__tab = np.zeros(bitmap.get_height() *  bitmap.get_width())
         for i in range(bitmap.get_width()):
             for j in range(bitmap.get_height()):
-                self.__tab.append(bitmap.get_cell_value(i, j))
+                self.__tab[i * bitmap.get_width() + j] = bitmap.get_cell_value(i, j)
--- feature/simple_features/min_feature.py
+++ feature/simple_features/min_feature.py
@@ -3,6 +3,7 @@
 """
 Moduł zawiera klasę wyliczajcą minimalna wartość koloru obrazka w skali szarości
 """
+import numpy as np
 from bitmap.bitmap_grayscale import BitmapGrayscale
 from feature import feature
 from bitmap import bitmap_grayscale
@@ -15,20 +16,23 @@ class MinFeature(feature.Feature):
     """
 
     def __init__(self):
-        self.__tab = []
+        self.__tab = None
 
     def calculate(self) -> float:
-        if len(self.__tab) == 0:
+        if self.__tab is None or len(self.__tab) == 0:
             raise RuntimeError("Run prepare() before calculate()")
 
         i = 0
         for cell in self.__tab:
             if cell < BitmapGrayscale.White:
+                self.__tab = None
                 return i
             i += 1
+        self.__tab = None
         return -1
 
     def prepare(self, bitmap: bitmap_grayscale) -> None:
+        self.__tab = np.zeros(bitmap.get_height() * bitmap.get_width())
         for i in range(bitmap.get_width()):
             for j in range(bitmap.get_height()):
-                self.__tab.append(bitmap.get_cell_value(i, j))
+                self.__tab[i * bitmap.get_width() + j] = bitmap.get_cell_value(i, j)
